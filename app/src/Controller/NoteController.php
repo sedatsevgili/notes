c4153eba9f26871b650412598aa94f74a16ce928
Sedat Sevgili <sedatsevgili@gmail.com>
2022-03-31 03:28:43 +0300

    remove redundant statement

--- app/src/Controller/NoteController.php
+++ app/src/Controller/NoteController.php
@@ -10,7 +10,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/api/notes')]
