4711f481066c5f55146ff24da6a6b59e69418330
Sedat Sevgili <sedatsevgili@gmail.com>
2022-04-01 06:56:54 +0300

    made vue run with docker

---
+++ app/assets/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="count++">Count is: {{ count }}</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
