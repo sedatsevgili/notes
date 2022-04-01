57acfa4f8dcafbeb061bf4aa73b1e6a086cc9c7f
Sedat Sevgili <sedatsevgili@gmail.com>
2022-04-01 06:58:54 +0300

    use composition api

--- app/assets/Index.vue
+++ app/assets/Index.vue
@@ -2,14 +2,21 @@
   <button @click="count++">Count is: {{ count }}</button>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      count: 0
-    }
-  }
+<script setup>
+import { ref, onMounted } from 'vue'
+
+// reactive state
+const count = ref(0)
+
+// functions that mutate state and trigger updates
+function increment() {
+  count.value++
 }
+
+// lifecycle hooks
+onMounted(() => {
+  console.log(`The initial count is ${count.value}.`)
+})
 </script>
 
 <style scoped>
